4c5ed05373074e60a09e2bc5be55f078e0c3ba18
beauwilliams <beau.williams@icloud.com>
2020-03-29 22:52:01 +1100

    Comitting my first python program!

---
+++ ExampleCodes/Python/Constant.py
@@ -0,0 +1,12 @@
+print("Starting...")
+
+import timeit
+
+code_to_test = """
+n = 10
+for i in range(0, int(n), 1):
+    print("i is " + str(i))
+"""
+
+elapsed_time = timeit.timeit(code_to_test, number=100)
+print("Took " + str(elapsed_time) + " seconds")
